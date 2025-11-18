da8b10273712af2bbfae732a9847923af6a9f4d4
viersss <vieryrraditio@gmail.com>
2025-11-18 19:11:29 +0700

    gajelas

--- resources/views/tutor/layout.blade.php
+++ resources/views/tutor/layout.blade.php
@@ -353,8 +353,7 @@
                 <nav class="navigation">
                     @php
                         $currentRoute = request()->route() ? request()->route()->getName() : null;
-
-                        $defaultMenuItems = [
+                        $menuItems = ($menuItems ?? []) ?: [
                             [
                                 'label' => 'Beranda',
                                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.6"><path stroke-linecap="round" stroke-linejoin="round" d="M4 4h6v6H4zM14 4h6v6h-6zM4 14h6v6H4zM14 14h6v6h-6z" /></svg>',
@@ -374,12 +373,8 @@
                                 'patterns' => ['tutor.quizzes.*'],
                             ],
                         ];
-
-                        if (!isset($menuItems) || !is_array($menuItems)) {
-                            $menuItems = $defaultMenuItems;
-                        }
                     @endphp
-                    @foreach ($menuItemsToRender as $item)
+                    @foreach ($menuItems as $item)
                         @php
                             $isActive = false;
                             foreach ($item['patterns'] as $pattern) {
