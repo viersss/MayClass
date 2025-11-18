dda9daf5c89c8ee7cca27fc8ae48a0454e53c360
vier <152761114+viersss@users.noreply.github.com>
2025-11-18 18:56:34 +0700

    Ensure tutor layout defines menu items

--- resources/views/tutor/layout.blade.php
+++ resources/views/tutor/layout.blade.php
@@ -354,10 +354,9 @@
                     @php
                         $currentRoute = request()->route() ? request()->route()->getName() : null;
 
-                        if (!isset($menuItems) || empty($menuItems)) {
-                            $menuItems = [
-                                [
-                                    'label' => 'Beranda',
+                        $menuItems = $menuItems ?? [
+                            [
+                                'label' => 'Beranda',
                                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.6"><path stroke-linecap="round" stroke-linejoin="round" d="M4 4h6v6H4zM14 4h6v6h-6zM4 14h6v6H4zM14 14h6v6h-6z" /></svg>',
                                 'route' => 'tutor.dashboard',
                                 'patterns' => ['tutor.dashboard'],
@@ -375,7 +374,6 @@
                                 'patterns' => ['tutor.quizzes.*'],
                             ],
                         ];
-                        }
                     @endphp
                     @foreach ($menuItems as $item)
                         @php
