b1ccc8f311265bd86b9eb0feb5b8cd358155657b
viersss <vieryrraditio@gmail.com>
2025-11-21 07:08:11 +0700

    VIER - ngubah scope if di schema biar jauh lebih aman

--- database/migrations/2025_11_20_064639_add_google_id_to_users_table.php
+++ database/migrations/2025_11_20_064639_add_google_id_to_users_table.php
@@ -11,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            // 1. Tambah kolom google_id setelah email
-            //hanya menambahkan kolom jika kolom tersebut belum ada
-            if (!Schema::hasColumn('users', 'google_id')) {
+        // 1. Cek dulu di luar, baru jalankan perintah tambah kolom
+        if (!Schema::hasColumn('users', 'google_id')) {
+            Schema::table('users', function (Blueprint $table) {
                 $table->string('google_id')->nullable()->after('email');
-            }
+            });
+        }
 
-            // 2. Ubah password jadi boleh kosong (nullable)
-            // Penting: karena user login Google tidak input password
+        // 2. Ubah password jadi nullable (terpisah biar rapi)
+        Schema::table('users', function (Blueprint $table) {
             $table->string('password')->nullable()->change();
         });
     }
@@ -29,13 +29,15 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            // Hapus kolom google_id
-             if (Schema::hasColumn('users', 'google_id')) {
+        // Cek dulu sebelum hapus
+        if (Schema::hasColumn('users', 'google_id')) {
+            Schema::table('users', function (Blueprint $table) {
                 $table->dropColumn('google_id');
-            }
+            });
+        }
 
-            // Kembalikan password jadi wajib diisi (tidak boleh null)
+        // Kembalikan password jadi wajib
+        Schema::table('users', function (Blueprint $table) {
             $table->string('password')->nullable(false)->change();
         });
     }
